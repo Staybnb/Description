37213ad1a2eca6269be19fb14d7faab59d8a764e
Matthew Jones <mjones61692@gmail.com>
2018-11-08 10:56:54 -0500

    updated server to use new model

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var model = require('./model/model.js');
-var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const model = require('./model/model.js');
+const path = require('path');
+const port = process.env.PORT || 7000;
 
-var app = express();
+const app = express();
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.all('/*', function(req, res, next) {
@@ -18,14 +19,6 @@ app.get('/description', function(req, res) {
 			res.send(results);
 		})
 		.catch((err) => console.log(err));
-  // db.selectAll(id, function(err, result) {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     var listing = JSON.parse(JSON.stringify(result));
-  //     res.send(listing);
-  //   }
-  // });
 });
 
 
@@ -33,7 +26,7 @@ app.get('/listing', function(req, res) {
   res.sendFile(path.join(__dirname, '/../react-client/dist/index.html'));
 });
 
-app.listen(process.env.PORT || 7000, function() {
-  console.log(`listening on port ${process.env.PORT || 7000}`);
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
 });
 
